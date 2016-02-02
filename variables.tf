6110618db8820e4ea91b01feedc770b755be4980
Mercer <dmercer@dmercer999.cvent.net>
2016-02-02 15:14:32 -0800

    Fixing typo

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "public_subnets" { default = "" }
 variable "private_subnets" { default = "" }
 variable "azs" { }
 variable "peer_name" { }
-variable "peer_owner_id" = { }
-variable "peer_vpc_id" = { }
+variable "peer_owner_id" { }
+variable "peer_vpc_id" { }
 variable "enable_dns_hostnames" {
   description = "should be true if you want to use private DNS within the VPC"
   default = false
