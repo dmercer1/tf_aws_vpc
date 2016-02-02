38523572744e2c9d8bb1d09ed4ebd28a6b7431de
Mercer <dmercer@dmercer999.cvent.net>
2016-02-02 15:19:04 -0800

    Fixing typo

--- variables.tf
+++ variables.tf
@@ -3,9 +3,6 @@ variable "cidr" { }
 variable "public_subnets" { default = "" }
 variable "private_subnets" { default = "" }
 variable "azs" { }
-variable "peer_name" { }
-variable "peer_owner_id" { }
-variable "peer_vpc_id" { }
 variable "enable_dns_hostnames" {
   description = "should be true if you want to use private DNS within the VPC"
   default = false
@@ -14,3 +11,6 @@ variable "enable_dns_support" {
   description = "should be true if you want to use private DNS within the VPC"
   default = false
 }
+variable "peer_name" { }
+variable "peer_owner_id" { }
+variable "peer_vpc_id" { }
