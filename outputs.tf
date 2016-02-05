159331c86ab0d8a5e47fe95c24fed5f9d3fb2e07
Mercer <dmercer@dmercer999.cvent.net>
2016-02-05 13:19:32 -0800

    Adding individual subnet outputs

--- outputs.tf
+++ outputs.tf
@@ -10,8 +10,20 @@ output "private_subnets" {
   value = "${join(",", aws_subnet.private.*.id)}"
 }
 
-output "public_subnets" {
-  value = "${join(",", aws_subnet.public.*.id)}"
+output "public_subnet_0" {
+  value = "${aws_subnet.private.0.id}"
+}
+
+output "public_subnet_1" {
+  value = "${aws_subnet.private.1.id}"
+}
+
+output "public_subnet_2" {
+  value = "${aws_subnet.private.2.id}"
+}
+
+output "public_subnet_3" {
+  value = "${aws_subnet.private.3.id}"
 }
 
 output "public_route_table_id" {
