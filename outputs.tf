82dd7da45a4fc67c202eab4d1d70cbe9b677e0b7
drew mercer <dmercer@crowdcompass.com>
2016-02-24 12:29:12 -0800

    Changing outputs

--- outputs.tf
+++ outputs.tf
@@ -6,24 +6,53 @@ output "vpc_id" {
   value = "${aws_vpc.mod.id}"
 }
 
-output "private_subnets" {
-  value = "${join(",", aws_subnet.private.*.id)}"
-}
-
-output "public_subnet_0" {
+output "private_subnet_0" {
   value = "${aws_subnet.private.0.id}"
 }
 
-output "public_subnet_1" {
+output "private_subnet_1" {
   value = "${aws_subnet.private.1.id}"
 }
 
-output "public_subnet_2" {
+output "private_subnet_2" {
   value = "${aws_subnet.private.2.id}"
 }
 
-output "public_subnet_3" {
+output "private_subnet_3" {
   value = "${aws_subnet.private.3.id}"
+}
+output "public_subnet_4" {
+  value = "${aws_subnet.private.4.id}"
+}
+
+output "public_subnet_5" {
+  value = "${aws_subnet.private.5.id}"
+}
+
+output "public_subnet_6" {
+  value = "${aws_subnet.private.6.id}"
+}
+
+output "public_subnet_7" {
+  value = "${aws_subnet.private.7.id}"
+}
+
+
+
+output "public_subnet_0" {
+  value = "${aws_subnet.public.0.id}"
+}
+
+output "public_subnet_1" {
+  value = "${aws_subnet.public.1.id}"
+}
+
+output "public_subnet_2" {
+  value = "${aws_subnet.public.2.id}"
+}
+
+output "public_subnet_3" {
+  value = "${aws_subnet.public.3.id}"
 }
 
 output "public_route_table_id" {
